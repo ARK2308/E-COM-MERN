3a6ffcb5d42196516305eb3bb5057f47c43e090a
ARK2308 <abdulark500@gmail.com>
2024-09-10 19:24:08 +0530

    Get Api Product , Pagination , Filter Category

--- server/controllers/product/productController.js
+++ server/controllers/product/productController.js
@@ -2,72 +2,120 @@ const categorydb = require("../../models/product/productCategoryModel");
 const cloudinary = require("../../Cloudinary/cloudinary");
 const productsdb = require("../../models/product/productModel");
 
-
-
 exports.addCategory = async (req, res) => {
-    const {    categoryname ,  description } =req.body
-    if(!categoryname || !description){
-        res.status(400).json({error:"Fill All Details"})
+  const { categoryname, description } = req.body;
+  if (!categoryname || !description) {
+    res.status(400).json({ error: "Fill All Details" });
+  }
+
+  try {
+    const existingcategory = await categorydb.findOne({
+      categoryname: categoryname,
+    });
+
+    if (existingcategory) {
+      res.status(400).json({ error: "This Category Already Exist" });
+    } else {
+      const addCategory = new categorydb({
+        categoryname,
+        description,
+      });
+
+      await addCategory.save();
+
+      res.status(200).json(addCategory);
     }
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
 
-    try {
-        const existingcategory = await categorydb.findOne({categoryname:categoryname});
+exports.getCategory = async (req, res) => {
+  try {
+    const getAllcategory = await categorydb.find();
+    res.status(200).json(getAllcategory);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
 
-        if(existingcategory){
-            res.status(400).json({error:"This Category Already Exist"});
-        }else{
-            const addCategory = new categorydb({
-                categoryname ,description
-            });
+// ADD PRODUCTS
+exports.addProduct = async (req, res) => {
+  const { categoryid } = req.query;
+  const file = req.file ? req.file.path : "";
+  const { productname, price, discount, quantity, description } = req.body;
 
-            await addCategory.save();
+  if (
+    !productname ||
+    !price ||
+    !discount ||
+    !quantity ||
+    !description ||
+    !file
+  ) {
+    res.status(400).json({ error: "all filed required" });
+  }
 
-            res.status(200).json(addCategory)
-        }
-    } catch (error) {
-        res.status(400).json(error)
+  try {
+    const upload = await cloudinary.uploader.upload(file);
+
+    const existingProduct = await productsdb.findOne({
+      productname: productname,
+    });
+
+    if (existingProduct) {
+      res.status(400).json({ error: "THis Product Already Exist" });
+    } else {
+      const addProduct = new productsdb({
+        productname,
+        price,
+        discount,
+        quantity,
+        description,
+        categoryid,
+        productimage: upload.secure_url,
+      });
+
+      await addProduct.save();
+      res.status(200).json(addProduct);
     }
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
 
+exports.getAllProducts = async (req, res) => {
+  const categoryid = req.query.categoryid || "";
+  const page = req.query.page || 1;
+  const ITEM_PER_PAGE = 8;
+
+  const query = {};
+  if(categoryid !== "all" && categoryid){
+    query.categoryid = categoryid
 }
 
+  try {
+    const skip = (page - 1) * ITEM_PER_PAGE; // (2-1)*8=8
 
-exports.getCategory = async(req ,res)=>{
-    try {
-        const getAllcategory = await categorydb.find();
-        res.status(200).json(getAllcategory)
-    } catch (error) {
-        res.status(400).json(error)
-        
-    }
-}
+    // product count
+    const count = await productsdb.countDocuments(query);
+    // console.log("count", count);
+    const getAllProducts = await productsdb
+      .find(query)
+      .limit(ITEM_PER_PAGE)
+      .skip(skip);
 
-// ADD PRODUCTS 
-exports.addProduct = async(req ,res)=>{
-    const {categoryid} = req.query;
-    const file = req.file ? req.file.path :""
-    const {productname,price,discount,quantity,description} = req.body;
+    //   page count
+    const pageCount = Math.ceil(count / ITEM_PER_PAGE);
 
-    if(!productname || !price || !discount || !quantity || !description || !file){
-        res.status(400).json({error:"all filed required"});
-    }
-
-    try {
-        const upload = await cloudinary.uploader.upload(file);
-
-        const existingProduct = await productsdb.findOne({productname:productname});
-
-        if(existingProduct){
-            res.status(400).json({error:"THis Product Already Exist"});
-        }else{
-            const addProduct = new productsdb({
-                productname,price,discount,quantity,description,categoryid,productimage:upload.secure_url
-            });
-
-            await addProduct.save();
-            res.status(200).json(addProduct)
-        }
-    } catch (error) {
-        res.status(400).json(error);
-    }
-   
-}
+    res.status(200).json({
+      getAllProducts,
+      Pagination: {
+        totalProducts: count,
+        pageCount,
+      },
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
--- server/routes/products/productroutes.js
+++ server/routes/products/productroutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const adminauthenticate = require("../../Middleware/admin/adminauthenticate");
-const {addCategory , getCategory , addProduct} = require("../../controllers/product/productController")
+const {addCategory , getCategory , addProduct ,getAllProducts} = require("../../controllers/product/productController")
 const productupload = require("../../Multerconfig/products/productStorageConfig");
 const router = new express.Router();
 
@@ -11,6 +11,7 @@ router.get("/getcategory" , getCategory)
 
 // products routes 
 router.post("/addproduct" , [adminauthenticate, productupload.single("productimage")], addProduct)
+router.get("/getproducts" , getAllProducts)
 
 
 
