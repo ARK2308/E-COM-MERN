dfd05ffbc6d9b145918ad045de4e9231602e2b60
ARK2308 <abdulark500@gmail.com>
2024-09-11 01:31:29 +0530

    Single Product , Latest and Delete Api lec-12

--- server/routes/products/productroutes.js
+++ server/routes/products/productroutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const adminauthenticate = require("../../Middleware/admin/adminauthenticate");
-const {addCategory , getCategory , addProduct ,getAllProducts} = require("../../controllers/product/productController")
+const {addCategory , getCategory , addProduct ,getAllProducts ,getSingleProduct ,getLatestProducts ,deleteProducts} = require("../../controllers/product/productController")
 const productupload = require("../../Multerconfig/products/productStorageConfig");
 const router = new express.Router();
 
@@ -12,8 +12,13 @@ router.get("/getcategory" , getCategory)
 // products routes 
 router.post("/addproduct" , [adminauthenticate, productupload.single("productimage")], addProduct)
 router.get("/getproducts" , getAllProducts)
+router.get("/getsingleproduct/:productid", getSingleProduct);
+router.delete("/deleteproducts/:productid",adminauthenticate, deleteProducts);
+
+// new arrival product
+router.get("/getlatestproducts", getLatestProducts);
 
 
 
-
+ 
 module.exports = router;
