c0ec80b084600e6ef3577d387c24bb910d1ed55f
ARK2308 <abdulark500@gmail.com>
2024-09-12 02:05:19 +0530

    Added the forgot password with token functionality and installed nodemailer| | added template (forgot template) || now adding the nodemailer path and fs system in this forgot password method

--- server/controllers/user/userControllers.js
+++ server/controllers/user/userControllers.js
@@ -1,93 +1,129 @@
-const  cloudinary  = require("../../Cloudinary/cloudinary");
+const cloudinary = require("../../Cloudinary/cloudinary");
 const userDB = require("../../models/user/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = "zhfgblkSNfKGncv"
+const path = require("path")
+const fs = require("fs")
+const ejs = require("ejs")
 
+exports.userRegister = async (req, res) => {
+  const { firstname, lastname, email, password, confirmpassword } = req.body;
 
+  if (
+    !firstname ||
+    !email ||
+    !lastname ||
+    !password ||
+    !confirmpassword ||
+    !req.file
+  ) {
+    res.status(400).json({ error: "all fileds are required" });
+  }
 
+  const file = req.file?.path;
+  const upload = await cloudinary.uploader.upload(file);
 
-exports.userRegister = async (req , res)=>{
-    const { firstname, lastname, email, password, confirmpassword } = req.body;
+  try {
+    const preuser = await userDB.findOne({ email: email });
 
-    if (!firstname || !email || !lastname || !password || !confirmpassword || !req.file) {
-        res.status(400).json({ error: "all fileds are required" })
+    if (preuser) {
+      res.status(400).json({ error: "this user is already exist" });
+    } else if (password !== confirmpassword) {
+      res
+        .status(400)
+        .json({ error: "password and confirm password not match" });
+    } else {
+      const userData = new userDB({
+        firstname,
+        lastname,
+        email,
+        password,
+        userprofile: upload.secure_url,
+      });
+
+      // here password hashing
+
+      await userData.save();
+      res.status(200).json(userData);
     }
-    
-    const file = req.file?.path;
-    const upload = await cloudinary.uploader.upload(file);
-    
-    try {
-        const preuser = await userDB.findOne({email:email});
-    
-        if(preuser){
-            res.status(400).json({error:"this user is already exist"});
-        }else if(password !== confirmpassword){
-            res.status(400).json({error:"password and confirm password not match"});
-        }else{
-            const userData = new userDB({
-                firstname, lastname, email, password, userprofile:upload.secure_url
-            });
-    
-            // here password hashing
-    
-            await userData.save();
-            res.status(200).json(userData);
-        }
-    } catch (error) {
-        res.status(400).json(error)
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+exports.userLogin = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json({ error: "all field require" });
+  }
+
+  try {
+    const userValid = await userDB.findOne({ email: email });
+    if (userValid) {
+      const isMatch = await bcrypt.compare(password, userValid.password);
+
+      if (!isMatch) {
+        res.status(400).json({ error: "Invalid Details" });
+      } else {
+        // token generate
+        const token = await userValid.generateuserAuthToken();
+
+        const result = {
+          userValid,
+          token,
+        };
+        res.status(200).json(result);
+      }
+    } else {
+      res.status(400).json({ error: "invalid details" });
     }
-}
-exports.userLogin = async (req , res)=>{
-    const {email,password} = req.body;
-    if(!email || !password){
-        res.status(400).json({error:"all field require"})
-    }
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
 
-    try {
-        const userValid = await userDB.findOne({email:email});
-        if(userValid){
-            const isMatch = await bcrypt.compare(password,userValid.password);
+exports.userVerify = async (req, res) => {
+  try {
+    const verifyUser = await userDB.findOne({ _id: req.userId });
+    res.status(200).json(verifyUser);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+exports.userLogout = async (req, res) => {
+  try {
+    req.rootUser.tokens = req.rootUser.tokens.filter((currentElement) => {
+      return currentElement.token !== req.token;
+    });
 
-            if(!isMatch){
-                res.status(400).json({error:"Invalid Details"})
-            }else{
+    req.rootUser.save();
+    res.status(200).json({ message: "user Succesfully Logout" });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+exports.forgotPassword = async (req, res) => {
+  const { email } = req.body;
 
-                // token generate
-                const token = await userValid.generateuserAuthToken();
+  if (!email) {
+    res.status(400).json({ error: "enter your email" });
+  }
 
-                const result = {
-                    userValid,
-                    token
-                }
-                res.status(200).json(result)
-            }
-        }else{
-            res.status(400).json({error:"invalid details"})
-        }
-    } catch (error) {
-        res.status(400).json(error)
-        
-    }
-}
+  try {
 
-exports.userVerify = async (req , res) => {
-    try {
-        const verifyUser = await userDB.findOne({_id:req.userId});
-        res.status(200).json(verifyUser)
-    } catch (error) {
-        res.status(400).json(error)
-        
-    }
-}
-exports.userLogout = async (req , res) => {
-    try {
-        req.rootUser.tokens = req.rootUser.tokens.filter((currentElement)=>{
-            return currentElement.token !== req.token
+    const userfind = await userDB.findOne({email:email});
+    if (userfind){
+        // token generate for password change
+        const token = jwt.sign({_id:userfind._id},SECRET_KEY,{
+            expiresIn:"120s"
         });
 
-        req.rootUser.save();
-        res.status(200).json({message:"user Succesfully Logout"})
-    } catch (error) {
-        res.status(400).json(error)
-        
+        const setusertoken = await userDB.findByIdAndUpdate({_id:userfind._id},{verifytoken:token},{new:true});
+        console.log("setusertoken" ,setusertoken)
+    }else{
+        res.status(400).json({error: "this user is not exist"})
     }
-}
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
--- server/routes/user/userAuthRoutes.js
+++ server/routes/user/userAuthRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const userUpload = require("../../Multerconfig/user/userStorageConfig")
-const { userRegister ,userLogin , userVerify , userLogout} = require("../../controllers/user/userControllers");
+const { userRegister ,userLogin , userVerify , userLogout ,forgotPassword} = require("../../controllers/user/userControllers");
 const userauthenticate = require("../../Middleware/user/userauthenticate")
 
 
@@ -10,6 +10,7 @@ router.post("/login" , userLogin)
 
 router.get("/userloggedin" ,userauthenticate , userVerify)
 router.get("/logout",userauthenticate, userLogout);
+router.post("/forgotpassword", forgotPassword);
 
 
 
