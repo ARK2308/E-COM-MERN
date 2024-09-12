8af0590397fdb1140cb539283c359b62bbd502a9
ARK2308 <abdulark500@gmail.com>
2024-09-13 03:42:25 +0530

    Decrement Items From Cart Api Completed

--- server/controllers/carts/cartsControllers.js
+++ server/controllers/carts/cartsControllers.js
@@ -1,74 +1,119 @@
 const productsdb = require("../../models/product/productModel");
 const cartsdb = require("../../models/carts/cartsModel");
 
-
 exports.addToCart = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-        const productfind = await productsdb.findOne({ _id: id });
+  try {
+    const productfind = await productsdb.findOne({ _id: id });
 
-        const carts = await cartsdb.findOne({ userid: req.userId, productid: productfind._id });
-        // console.log("productfind",productfind)
+    const carts = await cartsdb.findOne({
+      userid: req.userId,
+      productid: productfind._id,
+    });
+    // console.log("productfind",productfind)
 
-        if (productfind?.quantity >= 1) {
+    if (productfind?.quantity >= 1) {
+      if (carts?.quantity >= 1) {
+        // add to cart
+        carts.quantity = carts.quantity + 1;
+        await carts.save();
 
-            if (carts?.quantity >= 1) {
+        // decfrement product quantity
+        productfind.quantity = productfind.quantity - 1;
+        await productfind.save();
 
-                // add to cart
-                carts.quantity = carts.quantity + 1
-                await carts.save();
+        res
+          .status(200)
+          .json({ message: "Product Sucessfully Increment In your cart" });
+      } else {
+        const addtocart = new cartsdb({
+          userid: req.userId,
+          productid: productfind._id,
+          quantity: 1,
+        });
 
-                // decfrement product quantity
-                productfind.quantity = productfind.quantity - 1
-                await productfind.save();
+        await addtocart.save();
 
-                res.status(200).json({ message: "Product Sucessfully Increment In your cart" });
+        productfind.quantity = productfind.quantity - 1;
+        await productfind.save();
 
-
-            } else {
-                const addtocart = new cartsdb({
-                    userid: req.userId,
-                    productid: productfind._id,
-                    quantity: 1
-                });
-
-                await addtocart.save();
-
-                productfind.quantity = productfind.quantity - 1
-                await productfind.save();
-
-                res.status(200).json({ message: "Product Sucessfully Added In your cart" });
-            }
-        } else {
-            res.status(400).json({ error: "This Product Is Sold Out" });
-        }
-    } catch (error) {
-        res.status(400).json(error)
+        res
+          .status(200)
+          .json({ message: "Product Sucessfully Added In your cart" });
+      }
+    } else {
+      res.status(400).json({ error: "This Product Is Sold Out" });
     }
-}
-
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
 
 // getCartsValue
-exports.getCartsValue =async(req , res)=>{
-    try {
-        const getCarts = await cartsdb.aggregate([
-            {
-                $match: { userid: req.userMainId }
-            },
-            {
-                $lookup: {
-                    from: "productsmodels",
-                    localField: "productid",
-                    foreignField: "_id",
-                    as: "productDetails"
-                }
-            }
-        ]);
-        res.status(200).json(getCarts)
-    } catch (error) {
-        res.status(400).json(error)
+exports.getCartsValue = async (req, res) => {
+  try {
+    const getCarts = await cartsdb.aggregate([
+      {
+        $match: { userid: req.userMainId },
+      },
+      {
+        $lookup: {
+          from: "productsmodels",
+          localField: "productid",
+          foreignField: "_id",
+          as: "productDetails",
+        },
+      },
+    ]);
+    res.status(200).json(getCarts);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+// removeSingleiteam
+exports.removeSingleiteam = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const productfind = await productsdb.findOne({ _id: id });
+
+    const carts = await cartsdb.findOne({
+      userid: req.userId,
+      productid: productfind._id,
+    });
+
+    if (!carts) {
+      res.status(400).json({ error: "cart item not found" });
     }
 
-}
+    console.log("carts", carts);
 
+    if (carts.quantity == 1) {
+      const deleteCartItem = await cartsdb.findByIdAndDelete({
+        _id: carts._id,
+      });
+      // console.log("deleteCartItem",deleteCartItem)
+
+      // increment product quantity
+      productfind.quantity = productfind.quantity + 1;
+      await productfind.save();
+      res.status(200).json({
+        message: "Your Iteam sucessfully remove in your cart",
+        deleteCartItem,
+      });
+    } else if (carts.quantity > 1) {
+      carts.quantity = carts.quantity - 1;
+      await carts.save();
+
+      // increment product quantity
+      productfind.quantity = productfind.quantity + 1;
+      await productfind.save();
+      res
+        .status(200)
+        .json({ message: "Your Iteam Sucessfully Decrement In your Cart" });
+    }
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
--- server/routes/carts/cartsRoutes.js
+++ server/routes/carts/cartsRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const userauthenticate = require("../../Middleware/user/userauthenticate");
-const {addToCart ,getCartsValue } = require("../../controllers/carts/cartsControllers")
+const {addToCart ,getCartsValue ,removeSingleiteam } = require("../../controllers/carts/cartsControllers")
 const router = new express.Router();
 
 
 // carts routes
 router.post("/addtocart/:id",userauthenticate, addToCart);
 router.get("/getcarts",userauthenticate, getCartsValue);
+router.delete("/removesingleiteam/:id",userauthenticate, removeSingleiteam);
 
 
 
